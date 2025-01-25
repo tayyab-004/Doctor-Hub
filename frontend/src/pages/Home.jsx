a77deaa7e672486eceb91c3563f79c0acd3fe53b
tayyab-004 <tayyabgee333@gmail.com>
2025-01-25 16:09:00 +0500

    Adding Speciality Component to the Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SpecialityMenu />
     </div>
   );
 };
