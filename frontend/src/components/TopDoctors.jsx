62d130fbe868e84fd858ca8da9c3f257fc8ca4da
tayyab-004 <tayyabgee333@gmail.com>
2025-01-26 10:26:08 +0500

    Adding Context to the TopDoctors component

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { doctors } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
+
 const TopDoctors = () => {
   const navigate = useNavigate();
+  const { doctors } = useContext(AppContext);
 
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
