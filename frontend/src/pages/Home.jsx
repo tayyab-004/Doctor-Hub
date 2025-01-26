09da4d92cbcb9b3e996be5f65fa3478e6a87b93a
tayyab-004 <tayyabgee333@gmail.com>
2025-01-26 11:02:26 +0500

    Adding Banner Component to the Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import SpecialityMenu from "../components/SpecialityMenu";
 import TopDoctors from "../components/TopDoctors";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Header />
       <SpecialityMenu />
       <TopDoctors />
+      <Banner />
     </div>
   );
 };
