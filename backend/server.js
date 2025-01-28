966b9ef7668e5d498d9a49fe564dbc915d8bb64d
tayyab-004 <tayyabgee333@gmail.com>
2025-01-28 18:26:02 +0500

    Add: API to login and add-doctors by admin only
    Admin can login with token and only admin is able to add doctors.
    
    authAdmin middleware check admin credentials from .env file
    
    multer middleware upload the image file when admin add doctor image.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 
 // app config
 const app = express();
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 // API endpoint
+app.use("/api/admin", adminRouter);
 app.get("/", (req, res) => {
   res.send("API Working!");
 });
