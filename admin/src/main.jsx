af514bf46476c238444bc1a0dba164655fa42e18
tayyab-004 <tayyabgee333@gmail.com>
2025-01-29 17:03:00 +0500

    Add all the context to the main file of App
    Access all the context to the App.

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,9 +2,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import AdminContextProvider from "./context/AdminContext.jsx";
+import DoctorContextProvider from "./context/doctorContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AdminContextProvider>
+      <DoctorContextProvider>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </DoctorContextProvider>
+    </AdminContextProvider>
   </BrowserRouter>
 );
