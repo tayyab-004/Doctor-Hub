e763f74240df7e49c3e525d95d3af29c7cf49e02
tayyab-004 <tayyabgee333@gmail.com>
2025-01-29 18:14:10 +0500

    Connect Admin Login Page to the backend
    Admin is able to login with correct credentials. And when admin login we save token to the local storage and in state variable(setAToken). If we use wrong credentials then a error message is shown in toast. Add some values of token and backendUrl to the AdminContext.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,11 @@
 import Login from "./pages/Login";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   return (
     <div>
       <Login />
+      <ToastContainer />
     </div>
   );
 };
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,10 +1,40 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AdminContext } from "../context/AdminContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [state, setState] = useState("Admin");
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setAToken, backendUrl } = useContext(AdminContext);
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      // calling API according to state
+      if (state === "Admin") {
+        const { data } = await axios.post(backendUrl + "/api/admin/login", {
+          email,
+          password,
+        });
+
+        if (data.success) {
+          localStorage.setItem("aToken", data.token);
+          setAToken(data.token);
+        } else {
+          toast.error(data.message);
+        }
+      } else {
+      }
+    } catch (error) {}
+  };
+
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
         <p className="text-2xl font-semibold m-auto">
           <span className="text-primary">{state}</span> Login
@@ -12,6 +42,8 @@ const Login = () => {
         <div className="w-full">
           <p>Email</p>
           <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             className="border border-zinc-300 rounded w-full p-2 mt-1"
             type="email"
             required
@@ -20,6 +52,8 @@ const Login = () => {
         <div className="w-full">
           <p>Password</p>
           <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
             className="border border-zinc-300 rounded w-full p-2 mt-1"
             type="password"
             required
