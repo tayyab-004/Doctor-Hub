473b8ce56d03da717e33268ab4a794a8c55f772d
tayyab-004 <tayyabgee333@gmail.com>
2025-02-03 23:43:31 +0500

    Add: API to get all doctors list by admin only
    This API will fetch all the doctors list from database to the AllDoctor page of the frontend of admin panel.

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -127,4 +127,19 @@ const loginAdmin = async (req, res) => {
   }
 };
 
-export { addDoctor, loginAdmin };
+// API to get all the doctors list for admin pannel
+const allDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select("-password");
+    res.json({ success: true, doctors });
+  } catch (error) {
+    console.log(error);
+
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { addDoctor, loginAdmin, allDoctors };
