bf7fb02f0341127aeeed1de0c35ac638e9ab9a0b
tayyab-004 <tayyabgee333@gmail.com>
2025-02-04 23:55:52 +0500

    Add change availability functionality
    Now admin is able to change the availability of doctors.

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect } from "react";
 import { AdminContext } from "../../context/AdminContext";
 
 const DoctorsList = () => {
-  const { doctors, aToken, getAllDoctors } = useContext(AdminContext);
+  const { doctors, aToken, getAllDoctors, changeAvailability } = useContext(AdminContext);
 
   useEffect(() => {
     if (aToken) {
@@ -30,7 +30,7 @@ const DoctorsList = () => {
               </p>
               <p className="text-zinc-600 text-sm">{item.speciality}</p>
               <div className="mt-2 flex items-center gap-1 text-sm">
-                <input type="checkbox" checked={item.available} />
+                <input onChange={() => changeAvailability(item._id)} type="checkbox" checked={item.available} />
                 <p>Available</p>
               </div>
             </div>
