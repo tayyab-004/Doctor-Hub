a98037c5bf327f80873f7d5497398cc4a7874e13
tayyab-004 <tayyabgee333@gmail.com>
2025-02-27 17:30:55 +0500

    Add: API to get user appointments in Backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -173,4 +173,24 @@ const bookAppointment = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser, getProfile, updateProfile, bookAppointment };
+// API to get user Appointments
+const listAppointment = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const appointments = await appointmentModel.find({ userId });
+
+    res.json({ success: true, appointments });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export {
+  registerUser,
+  loginUser,
+  getProfile,
+  updateProfile,
+  bookAppointment,
+  listAppointment,
+};
