bb06e7ca3b5c482910b5e4ba309579a92119a6c0
tayyab-004 <tayyabgee333@gmail.com>
2025-02-28 17:59:35 +0500

    Add: API to verify payment of razorpay in Backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -259,6 +259,26 @@ const paymentRazorpay = async (req, res) => {
   }
 };
 
+// API to verify payment of razorpay
+const verifyRazorpay = async (req, res) => {
+  try {
+    const { razorpay_order_id } = req.body;
+    const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id);
+
+    if (orderInfo.status === "paid") {
+      await appointmentModel.findByIdAndUpdate(orderInfo.receipt, {
+        payment: true,
+      });
+      res.json({ success: true, message: "Payment successful!" });
+    } else {
+      res.json({ success: false, message: "Payment Failed!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 export {
   registerUser,
   loginUser,
@@ -268,4 +288,5 @@ export {
   listAppointment,
   cancelAppointment,
   paymentRazorpay,
+  verifyRazorpay,
 };
