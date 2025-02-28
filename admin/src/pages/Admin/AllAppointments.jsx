6c1cd2d4b6ce4f838de6b54c0b8d5e5f2e974519
tayyab-004 <tayyabgee333@gmail.com>
2025-02-28 23:34:41 +0500

    Add: All Appointments Page to the admin pannel

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -1,8 +1,32 @@
+import { useContext, useEffect } from "react";
+import { AdminContext } from "../../context/AdminContext";
 
 const AllAppointments = () => {
-  return (
-    <div>AllAppointments</div>
-  )
-}
+  const { aToken, appointments, getAllAppointments } = useContext(AdminContext);
 
-export default AllAppointments
+  useEffect(() => {
+    if (aToken) {
+      getAllAppointments();
+    }
+  }, [aToken]);
+
+  return (
+    <div className="w-full max-w-6xl m-5">
+      <p className="mb-3 text-lg font-medium">All Appointments</p>
+
+      <div className="bg-white border border-none rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll">
+        <div className="hidden sm:grid grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b">
+          <p>#</p>
+          <p>Patient</p>
+          <p>Age</p>
+          <p>Date & Time</p>
+          <p>Doctor</p>
+          <p>Fee</p>
+          <p>Actions</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllAppointments;
