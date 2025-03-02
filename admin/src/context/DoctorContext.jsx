70537e48d19b00a2d978c6771a0b4b8f253607e0
tayyab-004 <tayyabgee333@gmail.com>
2025-03-02 23:49:38 +0500

    Integrated complete and cancel appointment API with appointments page in Doctor Panel

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -14,12 +14,53 @@ const DoctorContextProvider = (props) => {
 
   const getAppointments = async () => {
     try {
-      const { data } = await axios.get(backendUrl + "/api/doctor/appointments", {
-        headers: { dToken },
-      });
+      const { data } = await axios.get(
+        backendUrl + "/api/doctor/appointments",
+        {
+          headers: { dToken },
+        }
+      );
       if (data.success) {
-        setAppointments(data.appointments.reverse());
-        console.log(data.appointments.reverse());
+        setAppointments(data.appointments);
+        console.log(data.appointments);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const completeAppointment = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/doctor/complete-appointment",
+        { appointmentId },
+        { headers: { dToken } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getAppointments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const cancelAppointment = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/doctor/cancel-appointment",
+        { appointmentId },
+        { headers: { dToken } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getAppointments();
       } else {
         toast.error(data.message);
       }
@@ -36,6 +77,8 @@ const DoctorContextProvider = (props) => {
     appointments,
     setAppointments,
     getAppointments,
+    completeAppointment,
+    cancelAppointment,
   };
 
   return (
