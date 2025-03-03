27adcf2c17db0fd4e427570c848cd35dd0c256e5
tayyab-004 <tayyabgee333@gmail.com>
2025-03-03 21:53:08 +0500

    Add: API to get the data on dashboard(doctor) in Backend

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -108,6 +108,41 @@ const appointmentCancel = async (req, res) => {
   }
 };
 
+// API to get dashboard data
+const doctorDashboard = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    const appointments = await appointmentModel.find({ docId });
+    let earnings = 0;
+
+    appointments.map((item) => {
+      if (item.isCompleted || item.payment) {
+        earnings += item.amount;
+      }
+    });
+
+    let patients = [];
+
+    appointments.map((item) => {
+      if (!patients.includes(item.userId)) {
+        patients.push(item.userId);
+      }
+    });
+
+    const dashData = {
+      earnings,
+      appointments: appointments.length,
+      patients: patients.length,
+      latestAppointments: appointments.reverse().slice(0, 5),
+    };
+
+    res.json({ success: true, dashData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 export {
   changeAvailability,
   doctorList,
@@ -115,4 +150,5 @@ export {
   appointmentsDoctor,
   appointmentComplete,
   appointmentCancel,
+  doctorDashboard,
 };
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -3,6 +3,7 @@ import {
   appointmentCancel,
   appointmentComplete,
   appointmentsDoctor,
+  doctorDashboard,
   doctorList,
   loginDoctor,
 } from "../controllers/doctorController.js";
@@ -15,5 +16,6 @@ doctorRouter.post("/login", loginDoctor);
 doctorRouter.get("/appointments", authDoctor, appointmentsDoctor);
 doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete);
 doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
+doctorRouter.get("/dashboard", authDoctor, doctorDashboard);
 
 export default doctorRouter;
